ac36c48fe6adefa7d67e03d2af4f69bbc7ba073f
Tom <tom@paybase.io>
2018-02-05 19:35:57 +0000

    get container working

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,8 @@ COPY . .
 RUN go get -d -v ./...
 RUN go install -v ./...
 
+COPY temp/translated_final.json /usr/src/book/harry-potter-1/fr/chapter-1/translated_final.json
+COPY temp/native_final.json /usr/src/book/harry-potter-1/fr/chapter-1/native_final.json
+ 
+EXPOSE 8080
 CMD ["app"]
--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -a
 source .env
 
 mkdir temp
-cp $CHAPTER_DIR/translated_final.json ./temp/translated_final.json
-cp $CHAPTER_DIR/native_final.json ./temp/native_final.json
+cp $CHAPTER_DIR/harry-potter-1/fr/chapter-1/translated_final.json ./temp/translated_final.json
+cp $CHAPTER_DIR/harry-potter-1/fr/chapter-1/native_final.json ./temp/native_final.json
 
 docker build -t ttollers/crayon-api ./
 
